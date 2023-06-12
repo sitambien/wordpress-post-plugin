2f9e8207f43b040dc1df7319f0cc00aaadc2e4af
Diego Fuentes <33032731+sitambien@users.noreply.github.com>
2023-06-11 21:22:43 -0400

    Add files via upload

--- wordpress-plugin-post.php
+++ wordpress-plugin-post.php
@@ -1,63 +1,63 @@
 <?php
 /*
 Plugin Name: Wordpress Post API
-Description: Adds an endpoint to create new posts through an external POST request with API key authentication.
-Version: 1.1
+Description: Endpoint para crear nuevas entradas por una solicitud POST externa.
+Version: 1.2
 Author: Diego Fuentes
 */
 
-// Genera la API key en el momento adecuado
+// Genera la API key
 add_action('plugins_loaded', 'generate_api_key');
 
 function generate_api_key() {
     define('MY_PLUGIN_API_KEY', "COMO.QUIERO123");
 }
 
-// Register the custom endpoint
+// Registro del endpoint
 add_action('rest_api_init', 'register_posts_endpoint');
 
 function register_posts_endpoint() {
     register_rest_route('posts/v1', 'crear', array(
         'methods' => 'POST',
         'callback' => 'create_new_post',
-        'permission_callback' => 'validate_api_key', // Custom permission callback
+        'permission_callback' => 'validate_api_key', // permisos
     ));
 }
 
-// Custom permission callback to validate API key
+// Callback para validar la clave de la API
 function validate_api_key($request) {
-    $api_key = $request->get_header('X-Api-Key'); // Get the API key from the request header
+    $api_key = $request->get_header('X-Api-Key'); // toma la API key del header
 
     if ($api_key === MY_PLUGIN_API_KEY) {
-        return true; // API key is valid, allow the request
+        return true;
     } else {
-        return false; // API key is invalid, deny the request
+        return false;
     }
 }
 
-// Callback to create a new post
+// Callback para crear la entrada
 function create_new_post($request) {
     $params = $request->get_params();
 
-    // Verifica los parámetros requeridos
+    // Verifica los parámetros
     if (empty($params['titulo']) || empty($params['contenido']) || empty($params['autor'])) {
         return new WP_Error('parametros_incompletos', 'Faltan parámetros requeridos.', array('status' => 400));
     }
 
-    // Get the author ID based on the provided author login or email
+    // Autor ID
     $author = get_user_by('login', $params['autor']);
     if (!$author) {
         $author = get_user_by('email', $params['autor']);
     }
 
-    // Crea el post
+    // Postea la entrada
     $post_data = array(
         'post_title' => sanitize_text_field($params['titulo']),
         'post_content' => wp_kses_post($params['contenido']),
         'post_status' => 'publish',
         'post_type' => 'post',
-        'post_author' => $author->ID, // Set the author ID
-        'tags_input' => explode(',', $params['etiquetas']), // Etiquetas separadas por coma (tag1,tag2,tag3)
+        'post_author' => $author->ID, // author ID
+        'tags_input' => explode(',', $params['etiquetas']), // Etiquetas separadas por coma
     );
 
     $post_id = wp_insert_post($post_data);
